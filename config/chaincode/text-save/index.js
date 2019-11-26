a68acc04708aedd4e6e20bfb2759eb0109eb0051
ShubhamKoli-VC <shubham.koli@viitor.cloud>
2019-11-26 14:29:57 +0530

    text-save chaincode and channel for it

---
+++ config/chaincode/text-save/index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const textSave = require('./lib/text_save');
+
+module.exports.textSave = textSave;
+module.exports.contracts = [textSave];
